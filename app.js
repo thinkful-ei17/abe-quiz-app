2c87454d98f0a11e75c2bd569b260f059a822f01
Rich Greenhill <rgnet1@yahoo.com>
2017-10-24 11:01:10 -0700

    all features

---
+++ app.js
@@ -0,0 +1,183 @@
+const TOP_LEVEL_COMPONENTS = ['js-intro', 'js-question', 'js-question-feedback', 'js-outro', 'js-quiz-status'];
+
+const QUESTIONS = [
+  { 
+    text: 'Capital of England?',
+    answers: [
+      'London', 'Paris', 'Rome', 'Washington DC'
+    ],
+    correctAnswer: 0
+  },
+  {
+    text: 'How many kilometers in one mile?',
+    answers: [
+      '0.6', '1.2', '1.6', '1.8'
+    ],
+    correctAnswer: 2
+  }
+];
+
+const getInitialStore = function(){
+  return {
+    page: 'intro',
+    currentQuestionIndex: null,
+    userAnswers: [],
+    feedback: null,
+  };
+};
+
+let store = getInitialStore();
+
+// Helper functions
+// ===============
+const hideAll = function() {
+  TOP_LEVEL_COMPONENTS.forEach(component => $(`.${component}`).hide());
+};
+
+const getScore = function() {
+  return store.userAnswers.reduce((accumulator, userAnswer, index) => {
+    const question = getQuestion(index);
+
+    if (question.correctAnswer === userAnswer) {
+      return accumulator + 1;
+    } else {
+      return accumulator;
+    }
+  }, 0);
+};
+
+const getProgress = function() {
+  return {
+    current: store.currentQuestionIndex + 1,
+    total: QUESTIONS.length
+  };
+};
+
+const getCurrentQuestion = function() {
+  return QUESTIONS[store.currentQuestionIndex];
+};
+
+const getQuestion = function(index) {
+  return QUESTIONS[index];
+};
+
+// HTML generator functions
+// ========================
+const generateAnswerItemHtml = function(answer, index) {
+  return `
+    <li class="answer-item">
+      <input type="radio" name="answers" value=${index} />
+      <span class="answer-text">${answer}</span>
+    </li>
+  `;
+};
+
+const generateQuestionHtml = function(question) {
+  return `
+    <form>
+      <div class="question-text">
+        ${question.text}      
+      </div>
+      <ul class="question-answers-list">
+        ${question.answers.map((answer, index) => generateAnswerItemHtml(answer, index)).join('')}
+      </ul>
+      <div>
+        <input type="submit" />
+      </div>
+    </form>
+  `;
+};
+
+const generateFeedbackHtml = function(feedback) {
+  return `
+    <p>
+      ${feedback}
+    </p>
+    <button class="continue js-continue">Continue</button>
+  `;
+};
+
+// Render function - uses `store` object to construct entire page every time it's run
+// ===============
+const render = function() {
+  let html;
+  hideAll();
+
+  const question = getCurrentQuestion();
+  const { feedback } = store; 
+  const { current, total } = getProgress();
+
+  $('.js-score').html(`<span>Score: ${getScore()}</span>`);
+  $('.js-progress').html(`<span>Question ${current} of ${total}`);
+
+  switch (store.page) {
+    case 'intro':
+      $('.js-intro').show();
+      break;
+    
+    case 'question':
+      html = generateQuestionHtml(question);
+      $('.js-question').html(html);
+      $('.js-question').show();
+      $('.quiz-status').show();
+      break;
+
+    case 'answer':
+      html = generateFeedbackHtml(feedback);
+      $('.js-question-feedback').html(html);
+      $('.js-question-feedback').show();
+      $('.quiz-status').show();
+      break;
+
+    case 'outro':
+      $('.js-outro').show();
+      $('.quiz-status').show();
+      break;
+  }
+};
+
+// Event handler functions
+// =======================
+const handleStartQuiz = function() {
+  store = getInitialStore();
+  store.page = 'question';
+  store.currentQuestionIndex = 0;
+  render();
+};
+
+const handleSubmitAnswer = function(e) {
+  e.preventDefault();
+  const question = getCurrentQuestion();
+  const selected = parseInt($('input:checked').val(), 10);
+  store.userAnswers.push(selected);
+  
+  if (selected === question.correctAnswer) {
+    store.feedback = 'You got it!';
+  } else {
+    store.feedback = `Too bad! The correct answer was: ${question.answers[question.correctAnswer]}`;
+  }
+
+  store.page = 'answer';
+  render();
+};
+
+const handleNextQuestion = function() {
+  if (store.currentQuestionIndex === QUESTIONS.length - 1) {
+    store.page = 'outro';
+    render();
+    return;
+  }
+
+  store.currentQuestionIndex++;
+  store.page = 'question';
+  render();
+};
+
+// On DOM Ready, run render() and add event listeners
+$(() => {
+  render();
+
+  $('.js-intro, .js-outro').on('click', '.js-start', handleStartQuiz);
+  $('.js-question').on('submit', handleSubmitAnswer);
+  $('.js-question-feedback').on('click', '.js-continue', handleNextQuestion);
+});
